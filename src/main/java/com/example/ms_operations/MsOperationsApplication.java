f1a6cbc0ecba8d17256762cc9ed8b2c70bb94516
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2026-05-01 10:27:38 -0500

    initial commit

---
+++ src/main/java/com/example/ms_operations/MsOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ms_operations/MsOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
